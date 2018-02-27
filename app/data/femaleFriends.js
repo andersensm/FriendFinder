69992f8043c7d611051eccca3f7d13556f914fa4
Sean Andersen <smandersen13@gmail.com>
2018-02-27 18:16:30 -0500

    updated male&female friends scores

--- app/data/femaleFriends.js
+++ app/data/femaleFriends.js
@@ -3,80 +3,80 @@ var femaleFriendsArray = [
     "name":"Scarlett Johansson",
     "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Scarlett_Johansson_in_Kuwait_01b-tweaked.jpg/220px-Scarlett_Johansson_in_Kuwait_01b-tweaked.jpg",
     "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
       ]
   },
   {
     "name":"Halle Berry",
     "photo":"https://i.pinimg.com/736x/93/ab/d1/93abd1aa743dba8895872cc3ebf49833--female-actresses-black-actresses.jpg",
     "scores":[
-        5,
-        1,
-        1,
-        1,
-        5,
-        1,
-        2,
-        2,
-        4,
-        1
+        "5",
+        "1",
+        "1",
+        "1",
+        "5",
+        "1",
+        "2",
+        "2",
+        "4",
+        "1"
       ]
   },
   {
     "name":"Jennifer Aniston",
     "photo":"https://cdn.gobankingrates.com/wp-content/uploads/2016/03/10_shutterstock_89307034-1-848x477.jpg",
     "scores":[
-        5,
-        1,
-        2,
-        2,
-        5,
-        3,
-        2,
-        2,
-        2,
-        1
+        "5",
+        "1",
+        "2",
+        "2",
+        "5",
+        "3",
+        "2",
+        "2",
+        "2",
+        "1"
       ]
   },
   {
     "name":"Emma Watson",
     "photo":"http://images.cdn.starpulse.com/7388449/980x.jpg",
     "scores":[
-        5,
-        3,
-        4,
-        3,
-        5,
-        5,
-        2,
-        5,
-        4,
-        5
+        "5",
+        "3",
+        "4",
+        "3",
+        "5",
+        "5",
+        "2",
+        "5",
+        "4",
+        "5"
       ]
   },
   {
     "name":"Jamie Chung",
     "photo":"https://pm1.narvii.com/6335/ca6d080cfbc8fd2f8417c8edbf70ab77f99ea299_hq.jpg",
     "scores":[
-        5,
-        2,
-        5,
-        4,
-        5,
-        2,
-        5,
-        5,
-        4,
-        3
+        "5",
+        "2",
+        "5",
+        "4",
+        "5",
+        "2",
+        "5",
+        "5",
+        "4",
+        "3"
       ]
   }
 ]
